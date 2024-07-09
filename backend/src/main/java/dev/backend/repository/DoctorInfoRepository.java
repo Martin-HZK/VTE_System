c814d99239921523a480597ebee889419338230e
Martin-HZK <ManUnitedHZK7@outlook.com>
2024-07-09 12:17:09 +0800

    Fix the bugs of project dependencies configuration

--- backend/src/main/java/dev/backend/repository/DoctorInfoRepository.java
+++ backend/src/main/java/dev/backend/repository/DoctorInfoRepository.java
@@ -1,15 +1,16 @@
 package dev.backend.repository;
 
 
-import dev.backend.model.DoctorInfo;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import dev.backend.model.DoctorInfo;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 
 @Repository
 public interface DoctorInfoRepository extends MongoRepository<DoctorInfo, ObjectId> {
+
 
     Optional<DoctorInfo> findDoctorInfoByName(String name);
 }
