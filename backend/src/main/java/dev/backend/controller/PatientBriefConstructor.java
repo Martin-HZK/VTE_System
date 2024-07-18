af708d2a71c680ef09b2cf3499c59eca90d4d5bb
martin <hzk@martindeMBP.ipads-lab.se.sjtu.edu.cn>
2024-07-18 21:00:47 +0800

    Create the API for getting all brief info

--- backend/src/main/java/dev/backend/controller/PatientBriefConstructor.java
+++ backend/src/main/java/dev/backend/controller/PatientBriefConstructor.java
@@ -1,2 +1,29 @@
-package dev.backend.controller;public class PatientBriefConstructor {
+package dev.backend.controller;
+
+
+import dev.backend.model.PatientBrief;
+import dev.backend.repository.PatientBriefService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/patient_brief")
+public class PatientBriefConstructor {
+    @Autowired
+    private PatientBriefService patientBriefService;
+
+    @GetMapping
+    public ResponseEntity<List<PatientBrief>> getAllPatientBrief() {
+        return new ResponseEntity<List<PatientBrief>>(patientBriefService.allBriefInfo(), HttpStatus.OK);
+    }
+
+
 }
--- backend/src/main/java/dev/backend/repository/PatientBriefService.java
+++ backend/src/main/java/dev/backend/repository/PatientBriefService.java
@@ -1,2 +1,24 @@
-package dev.backend.repository;public class PatientBriefService {
+package dev.backend.repository;
+
+import dev.backend.model.PatientBrief;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientBriefService {
+
+    @Autowired
+    private PatientBriefRepository patientBriefRepository;
+
+    public List<PatientBrief> allBriefInfo() {
+        return patientBriefRepository.findAll();
+    }
+
+
+
+
 }
